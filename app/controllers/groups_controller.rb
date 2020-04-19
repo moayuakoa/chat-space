f5b0cfe7782d6c215ce71b7a0c3b6b4b7ce6d425
moayuakoa <pinnnnno93@gmail.com>
2020-04-20 01:40:42 +0900

    add-message-Send-function-end

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  
   def index
   end
 
@@ -21,9 +22,9 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @gorup = Group.find(params[:id])
+    @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -32,7 +33,7 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name, user_ids: [])
+    params.require(:group).permit(:name, user_ids: [] )
   end
   
 end
