39919acd772a65703c05b6f7f7f70fbd0c3188b8
moayuakoa <pinnnnno93@gmail.com>
2020-04-24 09:10:09 +0900

    add-automatic-updating-done

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function() {
   function buildHTML(message) {
     if (message.image) {
       var html = 
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
             <div class="upper-message">
               <div class="upper-message__user-name">
                 ${message.user_name}
@@ -22,7 +22,7 @@ $(function() {
        return html;  
     } else {
       var html = 
-        `<div class="message">
+        `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -52,18 +52,44 @@ $('#new_message').on('submit', function(e) {
     dataType: 'json',
     processData: false,
     contentType: false
+  })
+  .done(function(data) {  
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('form')[0].reset();
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+  })
+    .fail(function () {
+      alert("error");
     })
-    .done(function(data) {  
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('form')[0].reset();
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+    .always(function (data) {
+      $('.form__submit').prop('disabled', false);
+    })
+})
+
+  var reloadMessages = function () { 
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function (messages) {
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+        }
       })
       .fail(function () {
-        alert("error");
-      })
-      .always(function (data) {
-        $('.form__submit').prop('disabled', false);
-      })
-  });
+        alert('error');
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000);
 });
